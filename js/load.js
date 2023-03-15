51feb74c252387b6277ddb3c7b9292ccddded9ad
kim365my <kim365my@knou.ac.kr>
2023-03-15 15:17:56 +0900

    완성

--- js/load.js
+++ js/load.js
@@ -1,2 +1,49 @@
-$(".header").load("./module/header.html");
+$(".header").load("./module/header.html", function(){
+    // -----------------
+    // snb 메뉴 열리게
+    // -----------------
+    // 메뉴 호버시 보이게
+    $(".menu").mouseenter(snbOpen);
+    $(".menu").keyup(snbOpen);
+
+    // -----------------
+    // header 메뉴를 떠나면 자동으로 닫히게
+    // -----------------
+    // XX snb 포지션을 업도미얼로 잡아서 헤더가 아니면 안됨 XX <- 된다!
+    // 메뉴를 호버해서 열리는 이벤트와 닫히는 이벤트를 같은 영역 안에서 사용해야 함. 
+    // 구조를 수정해야 하니까, .snb를 메뉴 안에 넣으면 메뉴 위에 호버하고 있는 시점에서는 사라지지 않음.
+    // 영역간의 블록을 잘 살펴야함
+    $(".menu").mouseleave(snbClose);
+
+    function snbOpen(){
+        $(".snb").stop().slideDown();
+    }
+    function snbClose(){
+        $('.snbTitle').removeClass(CHECK_CLASS);
+        $(".snb").stop().slideUp();
+    }
+    
+    // -----------------
+    // snb 제목을 클릭시 2뎁스가 보이게
+    // -----------------
+    // 클래스
+    const CHECK_CLASS = "check";
+
+    $(".snbTitle").click(function(){
+        // 다른 거 클릭하면 해당 요소말고 다른 요소는 다 클래스 제거
+        if($(this).hasClass(CHECK_CLASS)){
+            // $('.snbItem').hide();
+            $('.snbTitle').removeClass(CHECK_CLASS);
+        }else{
+            $('.snbTitle').removeClass(CHECK_CLASS);
+            $(this).addClass(CHECK_CLASS);
+        }
+    
+        /*$(".snbTitle").removeClass(CHECK_CLASS);
+        $(this).toggleClass(CHECK_CLASS); */
+        
+        // 클래스 토글
+        // console.log($(this).hasClass(CHECK_CLASS));
+    })
+});
 $(".footer").load("./module/footer.html");
